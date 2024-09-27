01422b603d45eaa916709c0a6107b0afa08a3ad2
GitYashm <yash571w1@gmail.com>
2024-09-27 19:06:20 +0530

    Update Tower.jsx

--- frontend/src/pages/Tower.jsx
+++ frontend/src/pages/Tower.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Item2data from "../Components/assets/item2data/item2data";
+import Item2data from "../Components/assets/item2data/Item2data";
 import TowerData from "../Components/assets/Tower/TowerData";
 
 const Tower = () => {
